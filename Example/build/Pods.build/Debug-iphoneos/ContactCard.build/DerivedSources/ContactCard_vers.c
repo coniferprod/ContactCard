5fad2f605fc315a38046d113feea5b337070b0d2
Jere Käpyaho <jere.kapyaho@solita.fi>
2017-03-14 18:20:16 +0200

    Removed ignored files

--- Example/build/Pods.build/Debug-iphoneos/ContactCard.build/DerivedSources/ContactCard_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char ContactCardVersionString[];
- extern const double ContactCardVersionNumber;
-
- const unsigned char ContactCardVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ContactCard  PROJECT:Pods-1" "\n";
- const double ContactCardVersionNumber __attribute__ ((used)) = (double)1.;
--- Example/build/Pods.build/Debug-iphoneos/Pods-ContactCard_Tests.build/DerivedSources/Pods_ContactCard_Tests_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char Pods_ContactCard_TestsVersionString[];
- extern const double Pods_ContactCard_TestsVersionNumber;
-
- const unsigned char Pods_ContactCard_TestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_ContactCard_Tests  PROJECT:Pods-1" "\n";
- const double Pods_ContactCard_TestsVersionNumber __attribute__ ((used)) = (double)1.;
