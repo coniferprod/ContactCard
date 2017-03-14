6b7a475d4b6947a2ff0a0edb5b002dcf495e46a1
Jere Käpyaho <jere@coniferproductions.com>
2017-03-14 20:54:59 +0200

    Beef up the example app

--- Example/ContactCard/ViewController.swift
+++ Example/ContactCard/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import ContactsUI
+import ContactCard
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CNContactPickerDelegate {
+    let controller = CNContactPickerViewController()
+    
+    @IBOutlet weak var pickContactButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        controller.delegate = self
+        
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,5 +29,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pickContact(_ sender: Any) {
+        navigationController?.present(controller, animated: true, completion: nil)
+    }
+    
+    func contactPickerDidCancel(_ picker: CNContactPickerViewController) {
+        print("Contact picking cancelled by user")
+    }
+    
+    func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
+        ContactAuthorizer.authorizeContacts { succeeded in
+            if succeeded {
+                let card = cardFromContact(contact: contact)
+            }
+        }
+    }
+}
+
+
+public final class ContactAuthorizer {
+    public class func authorizeContacts(completionHandler : @escaping (_ succeeded: Bool) -> Void) {
+        switch CNContactStore.authorizationStatus(for: .contacts) {
+        case .authorized:
+            completionHandler(true)
+            
+        case .notDetermined:
+            CNContactStore().requestAccess(for: .contacts) { succeeded, err in completionHandler(err == nil && succeeded)
+            }
+        default:
+            completionHandler(false)
+        }
+    }
 }
 
