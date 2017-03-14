62512ca52105a90dcc1ec1e64af7aac630d06675
Jere Käpyaho <jere.kapyaho@solita.fi>
2017-03-14 17:51:07 +0200

    Changes to gitignore

---
+++ Example/build/Pods.build/Debug-iphoneos/Pods-ContactCard_Example.build/DerivedSources/Pods_ContactCard_Example_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_ContactCard_ExampleVersionString[];
+ extern const double Pods_ContactCard_ExampleVersionNumber;
+
+ const unsigned char Pods_ContactCard_ExampleVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_ContactCard_Example  PROJECT:Pods-1" "\n";
+ const double Pods_ContactCard_ExampleVersionNumber __attribute__ ((used)) = (double)1.;
